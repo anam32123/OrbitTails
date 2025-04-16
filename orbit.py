36babc42fdf0e6ff4b7264e3435bfc735af3473c
anam32123 <53275327+anam32123@users.noreply.github.com>
2025-04-16 12:39:51 -0400

    Adding new plotting for streamlit and using session_state

--- orbit.py
+++ orbit.py
@@ -27,7 +27,7 @@ class Orbit:
         self.vx, self.vy, self.vz = self.orbit.v_xyz.to(u.km/u.s)
         self.t = self.orbit.t
 
-        return
+        return # don't need this
     
     def plot_orbit(self, *args, **kwargs):
         
@@ -115,6 +115,45 @@ class Orbit:
         # plt.show()
 
         return
+    
+    def plot_orbits_not_animated(self, time_index):
+        
+        fig, ax = plt.subplots(2, 3, figsize=(30, 20))
+
+        orbit_lines = []
+        galaxy_points = []
+        data_combos = [[self.x, self.y], [self.x, self.z], [self.y, self.z], [self.vx, self.vy], [self.vx, self.vz], [self.vy, self.vz]]
+
+        position_xlim, position_ylim = pu.find_limits_multiple_axes(data_combos[0:3], 0.05)
+        velocity_xlim, velocity_ylim = pu.find_limits_multiple_axes(data_combos[3:], 0.05)
+        
+        labels = [['$x$ (kpc)', '$y$ (kpc)'], ['$x$ (kpc)', '$z$ (kpc)'], ['$y$ (kpc)', '$z$ (kpc)'], ['$v_x$ (km/s)', '$v_y$ (km/s)'], ['$v_x$ (km/s)', '$v_z$ (km/s)'], ['$v_y$ (km/s)', '$v_z$ (km/s)']]
+
+        for i in range(3):
+            ax[0][i].set_xlim(position_xlim)
+            ax[0][i].set_ylim(position_ylim)
+            ax[1][i].set_xlim(velocity_xlim)
+            ax[1][i].set_ylim(velocity_ylim)
+
+        current_data = 0
+        time_labels = []
+        for row in ax:
+            for a in row:
+                a.text(0.05, 0.95, self.t[time_index], transform=a.transAxes, fontsize=12,
+                              verticalalignment='top', horizontalalignment='left', 
+                              bbox=dict(facecolor='white', alpha=0.5))
+                a.plot(0, 0, color="black", marker="x")
+                a.plot(data_combos[current_data][0][0], data_combos[current_data][1][0], 'x', color='red')
+                a.plot(data_combos[current_data][0][:time_index], data_combos[current_data][1][:time_index], alpha=0.75)
+                a.plot(data_combos[current_data][0][time_index], data_combos[current_data][1][time_index], '*', markersize=10, color='orange')
+                a.set_aspect('equal')
+                a.set_xlabel(labels[current_data][0])
+                a.set_ylabel(labels[current_data][1])
+
+                current_data += 1
+
+
+        return fig
     
     # def calc_2d_tail_angles_and_orbit:
 
--- web_app.py
+++ web_app.py
@@ -34,22 +34,55 @@ if coord_sys=='Cartesian':
     v_y0 = st.sidebar.number_input('$v_y$ (km/s)', value=1200)
     v_z0 = st.sidebar.number_input('$v_z$ (km/s)', value=100)
 
-host_potential = gp.NFWPotential.from_M200_c(M200 * u.Msun, c, units=galactic)
+if (
+    "host_potential" not in st.session_state or
+    st.session_state.M200 != M200 or
+    st.session_state.c != c
+):
+    st.session_state.host_potential = gp.NFWPotential.from_M200_c(M200 * u.Msun, c, units=galactic)
+    st.session_state.M200 = M200
+    st.session_state.c = c
+
+
+# host_potential = gp.NFWPotential.from_M200_c(M200 * u.Msun, c, units=galactic)
 st.write(f"Initialized cluster potential with Virial Radius {M200:.4e} and concentration {c}")
+# improve this grid
 x = np.linspace(-1000, 1000, 100)
 z = np.linspace(-1000, 1000, 100)
-fig = host_potential.plot_contours(grid=(x, 1., z))
+fig = st.session_state.host_potential.plot_contours(grid=(x, 1., z))
 st.write(fig)
 
 st.write('Calculating orbit...')
-initial_conditions = gd.PhaseSpacePosition(pos=[x0,y0,z0] * u.kpc,
-                                           vel=[v_x0,v_y0,v_z0] * u.km/u.s)
-galaxy_orbit = Orbit(host_potential, initial_conditions)
 
-fig = galaxy_orbit.plot_orbit(['x', 'y', 'z'])
-st.pyplot(fig)
-fig = galaxy_orbit.plot_orbit(['v_x', 'v_y', 'v_z'])
-st.pyplot(fig)
+if (
+    "intial_conditions" not in st.session_state or
+    st.session_state.x0 != x0 or
+    st.session_state.y0 != y0 or
+    st.session_state.z0 != z0 or
+    st.session_state.v_x0 != v_x0 or
+    st.session_state.v_y0 != v_y0 or
+    st.session_state.v_z0 != v_z0
+):
+    st.session_state.initial_conditions = gd.PhaseSpacePosition(pos=[x0,y0,z0] * u.kpc,
+                                           vel=[v_x0,v_y0,v_z0] * u.km/u.s)
+    st.session_state.galaxy_orbit = Orbit(st.session_state.host_potential, st.session_state.initial_conditions)
+    st.session_state.x0 = x0
+    st.session_state.y0 = y0
+    st.session_state.z0 = z0
+    st.session_state.v_x0 = v_x0
+    st.session_state.v_y0 = v_y0
+    st.session_state.v_z0 = v_z0
+
+# initial_conditions = gd.PhaseSpacePosition(pos=[x0,y0,z0] * u.kpc,
+#                                            vel=[v_x0,v_y0,v_z0] * u.km/u.s)
+initial_conditions = st.session_state.initial_conditions
+galaxy_orbit = st.session_state.galaxy_orbit
+# galaxy_orbit = Orbit(st.session_state.host_potential, initial_conditions)
+
+# fig = galaxy_orbit.plot_orbit(['x', 'y', 'z'])
+# st.pyplot(fig)
+# fig = galaxy_orbit.plot_orbit(['v_x', 'v_y', 'v_z'])
+# st.pyplot(fig)
 
 # trying the animation
 # galaxy_orbit.Animate_Orbit()
@@ -58,8 +91,12 @@ st.pyplot(fig)
 # calculate tail angles
 st.subheader('View 3D Tail Angles')
 galaxy_orbit.calc_3d_tail_angles()
-angle_time = st.slider('Time since initial conditions (Myr)', min_value=0., max_value=np.nanmax(galaxy_orbit.t).value, value=0.)
-st.write(f'3D tail angle unit vector: {galaxy_orbit.tail_angle_unit_vectors[:, angle_time]}')
+angle_time = st.select_slider('Time since initial conditions (Myr)', options=list(galaxy_orbit.t), value=0.)
+angle_time_index = list(galaxy_orbit.t).index(angle_time)
+fig = galaxy_orbit.plot_orbits_not_animated(angle_time_index)
+st.write('Hello?')
+st.pyplot(fig)
+st.write(f'3D tail angle unit vector: {galaxy_orbit.tail_angle_unit_vectors[:, angle_time_index]}')
 
-st.subheader('2D Tail Angle Projections')
-st.number_input('Viewing angle inclination ($^\circ$)', value=0, min_value=0, max_value=180)
+# st.subheader('2D Tail Angle Projections')
+# st.number_input('Viewing angle inclination ($^\circ$)', value=0, min_value=0, max_value=180)
